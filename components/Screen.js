51d8997ac3229ea2f20a8eeee97080a02e778601
Justin Tan <jtjtjt703@gmail.com>
2023-05-23 03:54:02 +0800

    refactored app background into Screen component

---
+++ components/Screen.js
@@ -0,0 +1,24 @@
+// Screen.js
+import React from "react";
+import { StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
+const Screen = ({ children }) => (
+  <LinearGradient
+    colors={["#3B82F6", "#06B6D4"]}
+    style={styles.gradient}
+    start={{ x: 0, y: 0 }}
+    end={{ x: 1, y: 0 }}
+  >
+    {children}
+  </LinearGradient>
+);
+
+const styles = StyleSheet.create({
+  gradient: {
+    flex: 1,
+    padding: 100,
+  },
+});
+
+export default Screen;
