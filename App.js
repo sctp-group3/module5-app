af34e0e9cdd6597e13c2bb4ccbbc76866fcaf68f
Justin Tan <jtjtjt703@gmail.com>
2023-05-22 18:47:25 +0800

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Platform } from "react-native";
+import React, { useState, useContext, useEffect } from "react";
+import SearchBar from "./components/SearchBar";
+import CurrentDisplay from "./components/CurrentDisplay";
+import WeatherContext from "./contexts/WeatherContext";
+import getUrl from "./services/weatherApi";
 
 export default function App() {
+  const [weatherData, setWeatherData] = useState(null);
+  const [targetCity, setTargetCity] = useState(null);
+
+  const ctx = useContext(WeatherContext);
+
+  const fetchWeatherData = async () => {
+    try {
+      const searchParam = {
+        ...targetCity,
+        units: ctx?.isMetric ? "metric" : "imperial",
+      };
+      const data = await getUrl(searchParam);
+      setWeatherData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (targetCity) {
+      fetchWeatherData();
+    }
+  }, [targetCity]);
+
+  useEffect(() => {
+    console.log("weatherData :", weatherData);
+  }, [weatherData]);
+
+  const onSubmit = (item) => {
+    setTargetCity(item);
+    console.log("weatherData :", weatherData);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar />
+      <View style={styles.header}>
+        <SearchBar onSubmit={onSubmit} />
+      </View>
+      <View style={styles.currentDisplayContainer1}>
+        <View style={styles.currentDisplayContainer2}>
+          {weatherData && <CurrentDisplay weather={weatherData.current} />}
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +58,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#b2e4f7",
+    alignItems: "center",
+  },
+  header: {
+    width: "100%",
+    backgroundColor: "#466995",
+    height: 120,
+  },
+  searchBarContainer: {
+    // accounting for status bar height on android
+    // Make sure searchBarContainer is on top
+  },
+  currentDisplayContainer1: {
+    flex: 1,
+    padding: 20,
+    alignContent: "center",
+    justifyContent: "center",
+  },
+  currentDisplayContainer2: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4,
+    padding: 15,
+    margin: 10, // React Native uses logical pixels, you might have to adjust the padding
+    backgroundColor: "#fff", // 75% white
+    opacity: 0.75,
   },
 });
---
+++ components/CurrentDisplay.js
@@ -0,0 +1,144 @@
+import React, { Fragment, useContext } from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
+import { Feather as Icon } from "@expo/vector-icons"; // or any suitable icon library
+import UnitsSelector from "./UnitsSelector";
+import WeatherContext from "../contexts/WeatherContext";
+
+const CurrentDisplay = ({ weather }) => {
+  const ctx = useContext(WeatherContext);
+  console.log("weather :", weather);
+
+  return (
+    <View style={styles.container}>
+      {weather && (
+        <Fragment>
+          <View style={styles.header}>
+            <View style={styles.titleContainer}>
+              <Text style={styles.title}>{weather.name}</Text>
+              <Text style={styles.description}>{weather.description}</Text>
+            </View>
+            <View style={styles.weatherContainer}>
+              <Image
+                source={{
+                  uri: `http://openweathermap.org/img/w/${weather.icon}.png`,
+                }}
+                style={styles.weatherIcon}
+              />
+              <View style={styles.temperatureContainer}>
+                <Text style={styles.temperature}>
+                  {weather.temp.toFixed(1)}
+                </Text>
+                {/* <UnitsSelector /> */}
+              </View>
+            </View>
+          </View>
+          <View style={styles.detailsContainer}>
+            <View style={styles.detailRow}>
+              <View style={styles.detailItem}>
+                <Icon name="arrow-up" style={styles.detailIcon} />
+                <Text style={styles.detailText}>
+                  High: {weather.temp_max.toFixed(1)}{" "}
+                  {ctx?.isMetric ? "°C" : "°F"}
+                </Text>
+              </View>
+              <View style={styles.detailItem}>
+                <Icon name="arrow-down" style={styles.detailIcon} />
+                <Text style={styles.detailText}>
+                  Low: {weather.temp_min.toFixed(1)}{" "}
+                  {ctx?.isMetric ? "°C" : "°F"}
+                </Text>
+              </View>
+            </View>
+            <View style={styles.detailItem}>
+              <Icon name="thermometer" style={styles.detailIcon} />
+              <Text style={styles.detailText}>
+                Feels like {weather.feels_like.toFixed(1)}{" "}
+                {ctx?.isMetric ? "°C" : "°F"}
+              </Text>
+            </View>
+            <View style={styles.detailItem}>
+              <Icon name="wind" style={styles.detailIcon} />
+              <Text style={styles.detailText}>
+                Wind {weather.speed.toFixed(1)} {ctx?.isMetric ? "m/s" : "ft/s"}
+              </Text>
+            </View>
+            <View style={styles.detailItem}>
+              <Icon name="droplet" style={styles.detailIcon} />
+              <Text style={styles.detailText}>
+                Humidity: {weather.humidity.toFixed(1)}%
+              </Text>
+            </View>
+          </View>
+        </Fragment>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  header: {
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: 10,
+  },
+  titleContainer: {
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "blue",
+  },
+  description: {
+    fontSize: 14,
+    fontWeight: "normal",
+    color: "blue",
+  },
+  weatherContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 10,
+  },
+  weatherIcon: {
+    width: 50,
+    height: 50,
+    marginRight: 10,
+  },
+  temperatureContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  temperature: {
+    fontSize: 60,
+    fontWeight: "normal",
+    marginRight: 5,
+  },
+  detailsContainer: {
+    marginTop: 10,
+    alignItems: "center",
+  },
+  detailRow: {
+    flexDirection: "row",
+    marginBottom: 10,
+  },
+  detailItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginRight: 20,
+  },
+  detailIcon: {
+    fontSize: 20,
+    marginRight: 5,
+  },
+  detailText: {
+    fontSize: 14,
+    fontWeight: "normal",
+  },
+});
+
+export default CurrentDisplay;
