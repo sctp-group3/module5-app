1c1e662eded59b2e2607238828c0af9fa41de604
OngJinHui <115149035+OngJinHui@users.noreply.github.com>
2023-05-24 13:24:00 +0800

    Update HourlyWeatherScreen.js

--- screens/HourlyWeatherScreen.js
+++ screens/HourlyWeatherScreen.js
@@ -1,13 +1,50 @@
 import React from "react";
 import { Text } from "react-native-paper";
 import Screen from "../components/Screen";
+import { StyleSheet, View, Image, FlatList } from 'react-native';
 
 const HourlyWeatherScreen = ({ weatherData }) => {
   return (
     <Screen>
-      <Text>Hourly Weather Screen</Text>
+      <Text style={styles.subtitle}>Hourly Forecast</Text>
+          <FlatList horizontal
+            data={weatherData.hourly.list}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={(hour) => {
+              const weather = hour.item.weather[0];
+              var dt = new Date(hour.item.dt * 1000);
+              return <View style={styles.hour}>
+                <Text>{dt.toLocaleTimeString().replace(/:\d+ /, ' ')}</Text>
+                <Text>{Math.round((hour.item.main.temp - 32) * 5/9)}°C</Text>
+                <Image
+                  style={styles.smallIcon}
+                  source={{
+                    uri: `http://openweathermap.org/img/wn/${weather.icon}@4x.png`,
+                  }}
+                />
+                <Text>{weather.description}</Text>
+              </View>
+            }}
+          />
     </Screen>
   );
 };
+
+const styles = StyleSheet.create({
+  subtitle: {
+    fontSize: 24,
+    marginVertical: 12,
+    marginLeft: 4,
+    color: '#e96e50',
+  },
+  hour: {
+    padding: 6,
+    alignItems: 'center',
+  },
+  smallIcon: {
+    width: 100,
+    height: 100,
+  }
+});
 
 export default HourlyWeatherScreen;
