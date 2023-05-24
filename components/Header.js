1ce26ffd41bcda7c783f6a5a4c07dce6e9cbf163
Ben Chan <benchan79@gmail.com>
2023-05-25 07:16:19 +0800

    Add prop and view for Favourites component

--- components/Header.js
+++ components/Header.js
@@ -1,28 +1,46 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
 import SearchBar from "./SearchBar";
 import CurrentLocationButton from "./CurrentLocationButton";
+import FavoritesDialog from "./FavouritesDialog";
 
-const Header = ({ onSubmit }) => {
+const Header = ({ onSubmit, targetCity }) => {
   return (
     <View style={styles.header}>
-      <View style={{ flex: 1 }}>
-        <SearchBar onSubmit={onSubmit} />
+      <View style={styles.topRow}>
+        <View style={styles.searchBarContainer}>
+          <SearchBar onSubmit={onSubmit} />
+        </View>
+        <CurrentLocationButton onSubmit={onSubmit} />
       </View>
-      <CurrentLocationButton onSubmit={onSubmit} />
+      <View style={styles.bottomRow}>
+        <FavoritesDialog onSubmit={onSubmit} targetCity={targetCity}/>
+      </View>
     </View>
-  );
-};
-
+  )
+}
 const styles = StyleSheet.create({
   header: {
     backgroundColor: "#F5F5F5",
     padding: 10,
-    justifyContent: "flex-start",
+    flexDirection: "column",
+    justifyContent: "center",
     alignItems: "center",
+    marginBottom: 10,
+  },
+  topRow: {
     flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     width: "100%",
   },
+  bottomRow: {
+    width: "100%",
+  },
+  searchBarContainer: {
+    width: "80%",
+  },
 });
+
 
 export default Header;
